6e2447ad8b55409a908e5065b7f26aa4d6d1f0ea
Aleksi Hulkkonen <kaa.hulkkonen@gmail.com>
2018-11-17 14:06:28 +0200

    NodeJS jutut gitignoreen, backend API / mysql testailua ja aloittelua.

---
+++ NodeJSRESTAPI/Tepla/routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	res.locals.connection.query('SELECT * from users', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+module.exports = router;
